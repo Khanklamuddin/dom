b589c408235de842b1e3024f8d782c2f83ad263d
Klamuddin Khan <kklamuddin90@gmail.com>
2025-02-05 22:40:41 +0530

    Add files via upload

---
+++ Day_22_DOM/app.js
@@ -0,0 +1,60 @@
+// let smallimg = document.getElementsByClassName("OldImg");
+
+// for(let i=0; i<smallimg.length; i++){
+//     console.dir(smallimg[i]);
+// }
+
+// QUERY SELECTOR //
+
+// console.dir(document.querySelector("p"));
+
+// console.dir(document.querySelector("#description"));
+// console.dir(document.querySelector(".OldImg"));
+
+// console.dir(document.querySelector("div a"));
+
+// console.dir(document.querySelectorAll("p"));
+
+// let link = document.querySelectorAll(".box a");
+
+// for(let i=0; i<link.length; i++){
+//     link[i].style.color = "purple";
+// }
+
+
+// PRACTICE QUESTION 1 //
+
+// let para = document.createElement("p");
+// para.innerText = "Hey I am red";
+// document.querySelector("body").append(para);
+
+// para.classList.add("red");
+
+// // PRACTICE QUESTION 2 //
+
+// let h3 = document.createElement("h3");
+// h3.innerText = "Hey I am blue";
+// document.querySelector("body").prepend(h3);
+
+// h3.classList.add("blue");
+
+// // PRACTICE QUESTION NO 3 //
+
+// let div = document.createElement("div");
+// let h1 = document.createElement("h1");
+// let para1 = document.createElement("para1");
+
+// h1.innerText = "I am in a div ";
+// para1.innerText = "ME TOO";
+
+// div.append(h1);
+// div.append(para1);
+
+// div.classList.add("box");
+
+// document.querySelector("body").prepend(div);
+
+
+
+
+
---
+++ Day_22_DOM/pq.app.js
@@ -0,0 +1,19 @@
+
+// PRACTICE QUESTION NO 1 // 
+let btn = document.createElement("btn");
+let input = document.createElement("input");
+btn.innerText = "Click me";
+
+document.querySelector("body").append(input);
+document.querySelector("body").append(btn);
+
+// PRACTICE QUESTION NO 2  //
+
+btn.setAttribute("id", "btn");
+input.setAttribute("placeholder", "username");
+
+// PRACTICE QUESTION NO 3 //
+
+// let btn = document.querySelector("#btn");
+// btn.classList.add("btnStyle");
+
